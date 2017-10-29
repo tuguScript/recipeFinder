a07f6983d4d6fb34165e14c31531d96bcc9e02a0
Tuguldur Bayarnyam <tuguldurbayarnyam@Tuguldurs-MacBook-Pro.local>
2017-10-29 04:07:55 -0700

    added search by tag

--- app/javascript/packs/App/App.jsx
+++ app/javascript/packs/App/App.jsx
@@ -1,6 +1,13 @@
 import React, { Component } from "react";
 import "./App.scss.css";
 import axios from "axios";
+import TagsInput from "react-tagsinput";
+
+const params =
+  "?mashape-key=3a6VknyIDEmshjDcEAPkhNr8FHxXp19URzajsnlWwvn2WYHTaW&limitLicense=true&number=10";
+const apiUrl =
+  "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/random" +
+  params;
 
 const Card = props => {
   return <div>{props.data.title}</div>;
@@ -10,29 +17,69 @@ export default class App extends Component {
   constructor() {
     super();
     this.state = {
-      recipes: [1, 2, 3, 4, 5]
+      recipes: [],
+      tags: []
     };
+    // this.getSearchResults = this.getSearchResults.bind(this);
   }
   componentDidMount() {
     const config = {
       headers: { "Access-Control-Allow-Origin": "*" }
     };
     axios
-      .get(
-        "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?mashape-key=3a6VknyIDEmshjDcEAPkhNr8FHxXp19URzajsnlWwvn2WYHTaW&fillIngredients=false&ingredients=carrot"
-      )
+      .get(apiUrl)
       .then(response => {
-        console.log(response.data);
-        this.setState({ recipes: response.data });
+        console.log(response);
+        this.setState({ recipes: response.data.recipes });
       })
       .catch(function(error) {
         console.log(error);
       });
   }
+  getSearchResults = e => {
+    // let { value } = e.target;
+    if (value.length < 3) return;
+    this.fetchSearchTerm(value);
+  };
+  // Get a new function that is debounced when called
+  // debouncedSearch = debounce(this.fetchSearchTerm, 700);
+  fetchSearchTerm = searchTerm => {
+    let api = `https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?fillIngredients=false&ingredients=${searchTerm}&limitLicense=false&number=10&ranking=1&mashape-key=3a6VknyIDEmshjDcEAPkhNr8FHxXp19URzajsnlWwvn2WYHTaW`;
+    // %2Csugar
+    // let searchparams = `?fillIngredients=false&ingredients=apple%2Csugar&limitLicense=false&number=5&ranking=1`;
+    // let api = apiUrl + searchparams;
+    axios
+      .get(api)
+      .then(response => {
+        console.log(response);
+        this.setState({ recipes: response.data });
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  };
+  handleChange(tags) {
+    console.log(
+      this.state.tags.map(tag => {
+        return tag;
+      })
+    );
+    this.setState({ tags }, () => {
+      this.fetchSearchTerm(
+        this.state.tags.map(tag => {
+          return tag.split(" ").join(",");
+        })
+      );
+    });
+  }
   render() {
     return (
       <div className="app">
-        App-ssssssss
+        <TagsInput
+          value={this.state.tags}
+          onChange={this.handleChange.bind(this)}
+        />
+        Random recipe
         {this.state.recipes.map((recipe, i) => {
           return <Card data={recipe} key={i} />;
         })}
